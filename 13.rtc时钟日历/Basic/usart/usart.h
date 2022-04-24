e459651c29447c6560e79b9d95d5322216350743
1loveman <911137865@qq.com>
2022-04-24 19:35:15 +0800

    2022.04.23

--- 13.rtc时钟日历/Basic/usart/usart.h
+++ 13.rtc时钟日历/Basic/usart/usart.h
@@ -1,13 +1,14 @@
 #ifndef __USART_H__
 #define __USART_H__
-
+#include<stdarg.h>
+#include<stdlib.h>
+#include<string.h>
+#include<stdio.h>
 #include"sys.h"
 
 #define USART_n USART1
 
 #define EN_USART1 1
-#define EN_USART2 0
-#define EN_USART3 0
 
 #define USART1_REC_LEN 200
 
@@ -16,7 +17,6 @@ extern u16 USART1_REC_STA;
 extern u8 USART1_REC_BUFF[USART1_REC_LEN];
 
 void USART1_Init(void);
-void USART1_Printf(char* fmt,...);
-
+void USART1_printf(char* fmt,...);
 
 #endif 
