287d439980e635c739e56687145226a6a62fb925
1loveman <911137865@qq.com>
2022-05-09 21:55:41 +0800

    20220509

--- 19.数码管/Hardware/TM1640.c
+++ 19.数码管/Hardware/TM1640.c
@@ -1,1 +1,42 @@
 #include"TM1640.h"
+
+void TM1640_Start(void){
+    GPIO_WriteBit(TM1640PORT,TM1640_SCLK,(BitAction)(1));
+    GPIO_WriteBit(TM1640PORT,TM1640_DIN,(BitAction)(1));
+    Delay_us(DEL);
+    GPIO_WriteBit(TM1640PORT,TM1640_DIN,(BitAction)(0));
+    Delay_us(DEL);
+    GPIO_WriteBit(TM1640PORT,TM1640_SCLK,(BitAction)(0));
+    Delay_us(DEL);
+}
+
+void TM1640_Stop(void){
+    GPIO_WriteBit(TM1640PORT,TM1640_SCLK,(BitAction)(1));
+    GPIO_WriteBit(TM1640PORT,TM1640_DIN,(BitAction)(0));
+    Delay_us(DEL);
+    GPIO_WriteBit(TM1640PORT,TM1640_DIN,(BitAction)(1));
+    Delay_us(DEL);
+}
+
+void TM1640_Write(u8 data){
+    int i=0;
+    GPIO_WriteBit(TM1640PORT,TM1640_SCLK,(BitAction)(0));
+    GPIO_WriteBit(TM1640PORT,TM1640_DIN,(BitAction)(0));
+    while((i++)<8){
+        GPIO_WriteBit(TM1640PORT,TM1640_SCLK,(BitAction)(0));
+        Delay_us(DEL);
+        if(data&0x01){
+            GPIO_WriteBit(TM1640PORT,TM1640_DIN,(BitAction)(1));
+            Delay_us(DEL);
+        }
+        else{
+            GPIO_WriteBit(TM1640PORT,TM1640_DIN,(BitAction)(0));
+            Delay_us(DEL);
+        }
+        GPIO_WriteBit(TM1640PORT,TM1640_SCLK,(BitAction)(1));
+        Delay_us(DEL);
+        data=data>>1;
+    }
+    GPIO_WriteBit(TM1640PORT,TM1640_SCLK,(BitAction)(0));
+    GPIO_WriteBit(TM1640PORT,TM1640_DIN,(BitAction)(0));
+}
--- 19.数码管/Hardware/TM1640.h
+++ 19.数码管/Hardware/TM1640.h
@@ -1,8 +1,16 @@
 #ifndef __TM1640_H__
 #define __TM1640_H__
 #include"stm32f10x.h"
+#include"delay.h"
+#define TM1640PORT GPIOA
+#define TM1640_SCLK GPIO_Pin_11
+#define TM1640_DIN GPIO_Pin_12
 #define TM1640_DSet 0x44
 #define TM1640_SSet 0x8D
-#define TM1640_ASet 
+#define DEL 1
+
+void TM1640_Init(void);
+void TM1640_Show(u8 address,u8 data);
+
 
 #endif
