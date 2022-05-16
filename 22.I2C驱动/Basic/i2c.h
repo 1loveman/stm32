5d0105b9fc1e1021fef2b81cbb9eada64ab76703
1loveman <911137865@qq.com>
2022-05-16 19:41:57 +0800

    20220516

--- 22.I2C驱动/Basic/i2c.h
+++ 22.I2C驱动/Basic/i2c.h
@@ -4,12 +4,13 @@
 #define I2C1PORT GPIOB
 #define I2C1_SDA GPIO_Pin_7
 #define I2C1_SCL GPIO_Pin_6
-#define HostAddress 0XC0
-#define BusSpeed 200000
+#define HOSTADDR 0XC0
+#define BUSSPEED 200000
 
-void I2C_Configuration(void);
-void I2C_Send_Byte(u8 SlaveAddr,u8 WriteAddr,u8 data);
-void I2C_Send_Buffer(u8 SlaveAddr,u8 WriteAddr,u8* BuffPtr,u16 BuffLen);
-u8 I2C_Read_Byte(u8 SlaveAddr,u8 ReadAddr);
-void I2C_Read_Buffer(u8 SlaveAddr,u8 ReadAddr,u8* BuffPtr,u16 BuffLen);
+void I2C1_Configuration(void);
+void I2C1_Send_Byte(u8 SlaveAddr,u8 WriteAddr,u8 Data);
+void I2C1_Send_Buffer(u8 SlaveAddr,u8 WriteAddr,u8* BufferPtr,u16 BufferLen);
+u8 I2C1_Read_Byte(u8 SlaveAddr,u8 ReadAddr);
+void I2C1_Read_Buffer(u8 SlaveAddr,u8 ReadAddr,u8* BufferPtr,u16 BufferLen);
+
 #endif
